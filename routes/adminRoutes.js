13add28b53e9f48e4bb688d4d4ec3afe07d18c6c
Albertamoahjunior <albertamoah2000@gmail.com>
2024-06-23 02:11:41 +0000

    made it compatible with the frontend

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,6 +5,7 @@ const adminController = require('../controllers/adminController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Public routes
+router.get('/check-auth', authMiddleware);
 router.post('/signup', adminController.signup);
 router.post('/login', adminController.login);
 router.post('/forgot-password', adminController.forgotPassword);
